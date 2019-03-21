b3b9ba3a7148679e98bba69b0ed329ed656e4daa
Michael Ruggiero <michaelruggiero@Michaels-MBP-3.verizon.net>
2019-03-20 21:44:33 -0400

    need to clean code, deleted some stuff for later

--- python/Project_Euler/*_factorization.py
+++
@@ -1,9 +0,0 @@
-def factors(n):    
-    result = set()
-    for i in range(2, int(n ** 0.5) + 1): #Change the first term to one if you want to include the factor 
-        div, mod = divmod(n, i)
-        if mod == 0:
-            result |= {i, div} #This symbol took me a while to figure. It means set = to union 
-    yield 1
-    for i in result:
-        yield i
