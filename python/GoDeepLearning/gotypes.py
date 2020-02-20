fb32d4d92935ec748a3e405b2b0e84cf3901fd6c
root <root@pop-os.localdomain>
2020-02-19 19:57:28 -0500

    updated golib

---
+++ python/GoDeepLearning/gotypes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # GoTypes Jupyter Notebook
+
+# This is a little notebook I made to follow along with the book "Deep Learning and the Game of Go" by Max Pumperla and Kevin Ferguson. All of these notebooks will get translated into .py files to run
+
+# In[ ]:
+
+
+
+
