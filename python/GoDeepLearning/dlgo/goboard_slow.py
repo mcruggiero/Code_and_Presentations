039b705cc6381c53cadfd85ff0f2c135d03bdda9
mcruggiero <michael@mcruggiero.com>
2020-02-21 11:30:15 -0500

    continuted to listing 3.8

--- python/GoDeepLearning/dlgo/goboard_slow.py
+++ python/GoDeepLearning/dlgo/goboard_slow.py
@@ -34,3 +34,44 @@ class Move():
         @classmethod
         def resign(cls):
             return Move(is_resign = True)
+        
+class GoString():  # <1>
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):  # <2>
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones)
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+    
+class Board():
+    
+    #We initialize an empty grid with the number of rows and columns
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {}
+        
+  
+        
